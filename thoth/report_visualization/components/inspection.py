11fc0d8db51ca8d6bdf36274cdf56dc5e6c0b9d3
Francesco Murdaca <fmurdaca@redhat.com>
2020-08-17 14:29:30 +0200

    Make pre-commit happy
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/report_visualization/components/inspection.py
+++ thoth/report_visualization/components/inspection.py
@@ -20,6 +20,14 @@
 import os
 import logging
 
+from typing import List
+
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.offline import iplot
+
+pd.options.plotting.backend = "plotly"
+
 # set up logging
 DEBUG_LEVEL = bool(int(os.getenv("DEBUG_LEVEL", 0)))
 
@@ -45,27 +53,25 @@ class AmunInspectionsVisualization:
         :param plot_df dataframe for plot of inspections results
         """
         if quantity not in _PERFORMANCE_QUANTITY:
-            logging.info(f"Only {_PERFORMANCE_QUANTITY} are accepted as quantity")
+            _LOGGER.info(f"Only {_PERFORMANCE_QUANTITY} are accepted as quantity")
             return
 
-        label_encoder = LabelEncoder()
-
-        X = [x[0] for x in plot_df[["re_string"]].values]
+        x_vector = [x[0] for x in plot_df[["re_string"]].values]
 
         integer_y_encoded = [y[0] for y in plot_df[["sws_hash_id_encoded"]].values]
 
-        Z = [z[0] for z in plot_df[[quantity]].values]
+        z_vector = [z[0] for z in plot_df[[quantity]].values]
 
         trace1 = go.Scatter3d(
-            x=X,
+            x=x_vector,
             y=integer_y_encoded,
-            z=Z,
+            z=z_vector,
             mode="markers",
             hovertext=[yc[0] for yc in plot_df[["sws_string"]].values],
             hoverinfo="text",
             marker=dict(
                 size=12,
-                color=Z,  # set color to an array/list of desired values
+                color=z_vector,  # set color to an array/list of desired values
                 colorscale="Viridis",  # choose a colorscale
                 opacity=0.8,
                 showscale=True,
@@ -78,7 +84,7 @@ class AmunInspectionsVisualization:
         annotations = []
         c = 0
 
-        for (x, y, z) in zip(X, integer_y_encoded, Z):
+        for (x, y, z) in zip(x_vector, integer_y_encoded, z_vector):
             annotations.append(
                 dict(
                     showarrow=False,
@@ -128,45 +134,45 @@ class AmunInspectionsVisualization:
 
         data = []
         annotations = []
-        c = 0
-        d = 0
+        colour_counter = 0
+        distance: float = 0
         for component in components:
             name_component = component
             if component == "pytorch":
                 name_component = "torch"
             subset_df = plot_df[plot_df["pi_component"] == component]
-            Z = [z[0] for z in subset_df[[quantity]].values]
+            z_vector = [z[0] for z in subset_df[[quantity]].values]
 
             trace = go.Scatter(
                 x=integer_y_encoded,
-                y=Z,
+                y=z_vector,
                 mode="markers",
                 hovertext=[y[0] for y in subset_df[["sws_string"]].values],
                 hoverinfo="text",
                 marker=dict(
                     size=12,
-                    color=Z,  # set color to an array/list of desired values
-                    colorscale=color_scales[c],  # choose a colorscale
+                    color=z_vector,  # set color to an array/list of desired values
+                    colorscale=color_scales[colour_counter],  # choose a colorscale
                     opacity=0.8,
                     showscale=True,
-                    colorbar={"x": 1 + d},
+                    colorbar={"x": 1 + distance},
                 ),
                 name=f"{component}==version",
             )
 
             data.append(trace)
-            c += 1
-            d += 0.2
+            colour_counter += 1
+            distance += 0.2
 
             if have_annotations:
-                for (yr, zr) in zip(integer_y_encoded, Z):
+                for (yr, zr) in zip(integer_y_encoded, z_vector):
                     annotations.append(
                         dict(
                             showarrow=False,
                             x=yr,
                             y=zr,
-                            text=f"{name_component}{subset_df[name_component].values[c][1]}, "
-                            + f"np{subset_df[name_component].values[c][1]}",
+                            text=f"{name_component}{subset_df[name_component].values[colour_counter][1]}, "
+                            + f"np{subset_df[name_component].values[colour_counter][1]}",
                             xanchor="left",
                             xshift=15,
                             opacity=0.7,
@@ -184,26 +190,3 @@ class AmunInspectionsVisualization:
         fig = go.Figure(data=data, layout=layout)
 
         iplot(fig, filename="scatter-colorscale")
-
-    @staticmethod
-    def create_inspection_analysis_plots(inspection_df: pd.DataFrame):
-        """Create inspection analysis plots for the inspection pd.Dataframe.
-
-        :param inspection_df: data frame as returned by `process_inspection_results' for a specific inspection identifier
-        """
-        # Box plots job duration and build duration
-        fig = create_duration_box(inspection_df, ["build_duration", "job_duration"])
-
-        py.iplot(fig)
-        # Scatter job duration
-        fig = create_duration_scatter(inspection_df, "job_duration", title="InspectionRun job duration")
-
-        py.iplot(fig)
-        # Scatter build duration
-        fig = create_duration_scatter(inspection_df, "build_duration", title="InspectionRun build duration")
-
-        py.iplot(fig)
-        # Histogram
-        fig = create_duration_histogram(inspection_df, ["job_duration"])
-
-        py.iplot(fig)
